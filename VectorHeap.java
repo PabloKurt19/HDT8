4c16ff4d58619c4a13683d3e3d52a772998c5d77
PabloKurt19 <pabloortiz57@gmail.com>
2016-10-02 21:44:05 -0600

    Clase VectorHeap
    Version 1.2 (Falta sobreescribir todos los metodos de vector)

--- VectorHeap.java
+++ VectorHeap.java
@@ -1,14 +1,38 @@
-public class VectorHeap<E extends Comparable<E>> implements PriorityQueue<E>{
-	protected Vector<E> data; // the data, kept in heap order
+import java.util.Vector;
 
-    
+/**
+ *Vector-Based Heaps
+ */
+
+/**
+ * @author Pablo Ortiz
+ * @author Pedro García
+ * @version 01.10.16
+ */
+
+//Código extraido del libro Java Structures
+
+public class VectorHeap<E extends Comparable<E>> implements PriorityQueue<E>{
+
+	/**
+	 * atributo
+	 */
+    protected Vector<E> data; // the data, kept in heap order
+
+    /**
+     * Constructor
+     */
     public VectorHeap()
 	// post: constructs a new priority queue
 	{
 		data = new Vector<E>();
 	}
-	
-	 public VectorHeap(Vector<E> v)
+
+    /**
+     * Constructor con parametros
+     * @param Vector<E> v
+     */
+    public VectorHeap(Vector<E> v)
 	// post: constructs a new priority queue from an unordered vector
 	{
 		int i;
@@ -18,29 +42,48 @@ public class VectorHeap<E extends Comparable<E>> implements PriorityQueue<E>{
 			add(v.get(i));
 		}
 	}
-	
-	 protected static int parent(int i)
+
+    /**
+     * @description devuelve el padre del nodo a evaluado
+     * @param int i
+     * @return int
+     */
+    protected static int parent(int i)
 	// pre: 0 <= i < size
 	// post: returns parent of node at location i
 	{
 		return (i-1)/2;
 	}
-	
-	protected static int left(int i)
+
+    /**
+     * @description devuelve el hijo de la izquierda del nodo a evaluado
+     * @param i
+     * @return int
+     */
+    protected static int left(int i)
 	// pre: 0 <= i < size
 	// post: returns index of left child of node at location i
 	{
 		return 2*i+1;
 	}
-	
-	protected static int right(int i)
+
+    /**
+     * @description devuelve el hijo de la derecha del nodo a evaluado
+     * @param i
+     * @return int
+     */
+    protected static int right(int i)
 	// pre: 0 <= i < size
 	// post: returns index of right child of node at location i
 	{
 		return (2*i+1) + 1;
 	}
-	
-	protected void percolateUp(int leaf)
+
+    /**
+     * @description mueve el último nodo (hoja) a la posición adecuada
+     * @param leaf
+     */
+    protected void percolateUp(int leaf)
 	// pre: 0 <= leaf < size
 	// post: moves node at index leaf up to appropriate position
 	{
@@ -55,6 +98,69 @@ public class VectorHeap<E extends Comparable<E>> implements PriorityQueue<E>{
 		}
 		data.set(leaf,value);
 	}
-	
-	
-}
+
+    /**
+     * @description le agrega contenido a un nodo del vecotr
+     * @param E value
+     */
+    public void add(E value)
+	// pre: value is non-null comparable
+	// post: value is added to priority queue
+	{
+		data.add(value);
+		percolateUp(data.size()-1);
+	}
+
+    /**
+     * @description mueve una posicion abajo a la raiz
+     * @param int root
+     */
+    protected void pushDownRoot(int root)
+	// pre: 0 <= root < size
+	// post: moves node at index root down
+	// to appropriate position in subtree
+	{
+		int heapSize = data.size();
+		E value = data.get(root);
+		while (root < heapSize) {
+			int childpos = left(root);
+			if (childpos < heapSize)
+			{
+				if ((right(root) < heapSize) &&
+				((data.get(childpos+1)).compareTo
+				(data.get(childpos)) < 0))
+				{
+					childpos++;
+				}
+			// Assert: childpos indexes smaller of two children
+			if ((data.get(childpos)).compareTo
+				(value) < 0)
+			{
+				data.set(root,data.get(childpos));
+				root = childpos; // keep moving down
+			} else { // found right location
+				data.set(root,value);
+				return;
+			}
+			} else { // at a leaf! insert and halt
+				data.set(root,value);
+				return;
+			}
+		}
+	}
+
+    /**
+     * @description elimina el ultimo nodo del vector y devuelve el mismo
+     * @return Element
+     */
+    @Override
+	public E remove()
+	// pre: !isEmpty()
+	// post: returns and removes minimum value from queue
+	{
+		E minVal = getFirst();
+		data.set(0,data.get(data.size()-1));
+		data.setSize(data.size()-1);
+		if (data.size() > 1) pushDownRoot(0);
+		return minVal;
+	}
